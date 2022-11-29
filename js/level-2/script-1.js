5d18ee39c7b01514da7970f69717fe7bbba3dc2f
GunaRakulan-GR <gunarakulan@gmail.com>
2022-11-29 22:25:26 +0530

    chore: cleanup

--- js/level-2/script-1.js
+++ js/level-2/script-1.js
@@ -52,7 +52,7 @@ $(window).on('load', function(){
           
             if (isPuzzleMode == false) {
 
-                console.log($div1.attr("id"))
+               
                 DoorOpenPuzzleFunction($div1, doortype, TimeDuration);
             }
 
