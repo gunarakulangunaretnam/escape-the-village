123e9b06c0f3f908b0d5002bda5bdd94f30d40aa
GunaRakulan-GR <gunarakulan@gmail.com>
2022-11-30 11:13:57 +0530

    chore: reduce doors to 15 in level 2

--- python-scripts/door-generator.py
+++ python-scripts/door-generator.py
@@ -7,22 +7,19 @@ f = open("output-html.txt", 'w')
 f1 = open("output-js.txt", "w+")
 f1 = open("output-js.txt", 'w')
 
-for i in range(25):
+
+x = 2000
+
+for i in range(18):
     startfrom = startfrom + 1
-    x = random.randint(2000, 34000)
-    types = ["cactus-1.png","cactus-2.png"]
 
     doortype = random.randint(1, 4)
-
-
-
    
     f.write(f'<img src="images/doors/1.png" style="position:absolute; left:{x}px; top:-175px; height: 480px;" id="door{startfrom}"/> \n' )
     
-    
     f1.write(f'''if( $("#door{startfrom}").length)  DoorLogic($("#door{startfrom}"), $("#div2"), "door-open-box-{doortype}", 0); \n''')
 
-     
+    x = x + 2000
 
 f.close()
 f1.close()
