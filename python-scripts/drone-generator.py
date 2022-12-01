6b2d8a3517868c0e96d481be71f8e3e5271328ea
GunaRakulan-GR <gunarakulan@gmail.com>
2022-12-01 20:28:09 +0530

    chore: decrease drones in level 6

--- python-scripts/drone-generator.py
+++ python-scripts/drone-generator.py
@@ -9,12 +9,12 @@ f1 = open("output-js.txt", 'w')
 
 x = 500
 
-for i in range(30):
+for i in range(15):
     startfrom = startfrom + 1
 
     f.write(f'<img src="images/level-6-assets/Objects/drone.gif" class="droneAnimation" style="position:absolute; left:{x}px; top:-550px; width: 300px;" id="drone{startfrom}" /> \n')
 
-    x = x + 1200
+    x = x + 2500
 
     
 
