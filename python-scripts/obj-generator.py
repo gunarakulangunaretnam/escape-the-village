9d52befe8f19af00db020589cae9c0bd1a4f3dcd
GunaRakulan-GR <gunarakulan@gmail.com>
2022-12-01 21:14:33 +0530

    chore: update drone flying direction

--- python-scripts/obj-generator.py
+++ python-scripts/obj-generator.py
@@ -7,11 +7,13 @@ f = open("output-html.txt", 'w')
 f1 = open("output-js.txt", "w+")
 f1 = open("output-js.txt", 'w')
 
+arr = [1,2]
 
-for i in range(15):
+for i in range(20):
     startfrom = startfrom + 1
+    x = random.randint(2000, 38000)
 
-    f.write(f'<img src="images/level-6-assets/Objects/drone.gif" class="droneAnimation" style="position:absolute; left:{x}px; top:-550px; width: 300px;" id="drone{startfrom}" /> \n')
+    f.write(f'<img src="images/level-6-assets/Objects/{random.choice(arr)}.png" class="" style="position:absolute; left:{x}px; top:-30px; width: 40px;" id="obj{startfrom}"/> \n')
 
 
     
