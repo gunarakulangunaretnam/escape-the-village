3b201ad6d3a054cc7821ff1f26e491215c6a22d3
GunaRakulan-GR <gunarakulan@gmail.com>
2022-12-01 20:56:56 +0530

    chore: add backgorund for level 6

---
+++ python-scripts/obj-generator.py
@@ -0,0 +1,20 @@
+import random
+startfrom = 0
+
+f = open("output-html.txt", "w+")
+f = open("output-html.txt", 'w')
+
+f1 = open("output-js.txt", "w+")
+f1 = open("output-js.txt", 'w')
+
+
+for i in range(15):
+    startfrom = startfrom + 1
+
+    f.write(f'<img src="images/level-6-assets/Objects/drone.gif" class="droneAnimation" style="position:absolute; left:{x}px; top:-550px; width: 300px;" id="drone{startfrom}" /> \n')
+
+
+    
+
+f.close()
+f1.close()
