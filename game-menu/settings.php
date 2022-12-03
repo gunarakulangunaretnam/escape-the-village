362ae7de16119450c1bd5d650194e18c9cd6ba49
GunaRakulan-GR <gunarakulan@gmail.com>
2022-12-03 23:58:04 +0530

    feat: create a setting page in game menu

---
+++ game-menu/settings.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+
+<head>
+    <title>Escape The Village</title>
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="theme-color" content="hsl(221 90% 98%)" media="(prefers-color-scheme: light)">
+    <meta name="theme-color" content="Canvas" media="(prefers-color-scheme: dark)">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script type="module" src="arrowkey-support.js"></script>
+    <script type="module" src="mouse-parallax.js"></script>
+</head>
+
+<body>
+
+    <div class="jumbotron glasseffect" style="width: 1000px !important; ">
+        <h1 style="text-align:center;  font-family: Audiowide !important; font-weight:bold;">Settings</h1>
+    </div>
+
+    <div class="jumbotron glasseffect">
+
+
+        <h2 style="font-family: Audiowide !important; color:black; font-weight:bold;">Change Password</h2>
+
+        <hr>
+
+        <form>
+            <div class="form-group">
+                <label for="currentPassword"
+                    style="color:black; font-size: 22px; font-weight:bold; text-shadow: 1px 1px 4px red;">Current
+                    Password</label>
+                <input type="email" class="form-control effectForTextBoxes" id="currentPassword" name="currentPassword"
+                    aria-describedby="emailHelp" placeholder="Current Password">
+
+            </div>
+
+            <div class="form-group">
+                <label for="newPassword"
+                    style="color:black; font-size: 22px; font-weight:bold; text-shadow: 1px 1px 4px red;">New
+                    Password</label>
+                <input type="email" class="form-control effectForTextBoxes" id="newPassword" name="newPassword"
+                    aria-describedby="emailHelp" placeholder="New Password">
+
+            </div>
+
+            <div class="form-group">
+                <label for="confirmPassword"
+                    style="color:black; font-size: 22px; font-weight:bold; text-shadow: 1px 1px 4px red;">Confirm
+                    Password</label>
+                <input type="email" class="form-control effectForTextBoxes" id="confirmPassword" name="confirmPassword"
+                    aria-describedby="emailHelp" placeholder="Confirm Password">
+
+            </div>
+
+            <a type="submit" href="game-dashboard.php" class="btn btn-danger effectForButtonBlue">
+                < Go Back</a>
+
+                    <input type="submit" style="float:right;" class="btn btn-success effectForButtonGreen
+" value="Submit">
+        </form>
+
+    </div>
+
+    <div class="jumbotron glasseffect" style="width: 1000px !important; ">
+        <h1 style="text-align:center;  font-family: Audiowide !important; font-weight:bold;">
+            Reset The Game
+        </h1>
+
+        <p class="lead" style="font-weight:bold;"><span
+                style="color:red; font-weight:900; text-shadow: 2px 2px 3px black;">WARNING: </span>Resting the game
+            will delete all the finished levels and scores and create a new game to play from scratch.</p>
+
+        <form>
+
+            <button type="button" class="btn btn-danger btn-lg btn-block effectForButtonRed">RESET THE GAME</button>
+
+        </form>
+    </div>
+
+    <script src="" async defer></script>
+</body>
+
+</html>
